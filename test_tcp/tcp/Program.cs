73de8c395658cab686da89a323f022335d7006a5
521dq <duqing@qinyuantec.com>
2021-05-17 17:02:03 +0800

    first commit

---
+++ test_tcp/tcp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace tcp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string m_ip;
+            Console.WriteLine("Input socket ip:");
+            m_ip = Console.ReadLine();
+            Console.WriteLine("output socket ip: {0}", m_ip);
+            Console.ReadKey();
+        }
+    }
+}
